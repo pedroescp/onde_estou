cde9f55fcecddd3dbaafda2bc293d884847d66c2
pedroescp <pedrohhss14@gmail.com>
2024-01-05 21:38:11 +0000

    Feat: adptation the code for use "onde estou"

---
+++ onde_estou_app/resources/views/onde_estou.blade.php
@@ -0,0 +1,32 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('onde_estou') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    {{ __("You're logged in!") }}
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                <div class="shrink-0 flex items-center">
+                    <a href="{{ route('onde_estou') }}">
+                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                    </a>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</x-app-layout>
