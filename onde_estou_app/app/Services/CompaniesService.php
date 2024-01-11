4c0bf31ab9518be1bebb6c239ded062149e48654
pedroescp <pedrohhss14@gmail.com>
2024-01-11 11:23:27 +0000

    Feat: create a service

---
+++ onde_estou_app/app/Services/CompaniesService.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Services;
+
+use App\DTO\CreateCompaniesDTO;
+use App\DTO\UpdateCompaniesDTO;
+use App\Repositories\CompaniesRepositoriesInterface;
+use stdClass;
+
+class CompaniesService
+{
+    public function __construct(protected CompaniesRepositoriesInterface $repository)
+    {
+        
+    }
+
+    public function getAll(string $filter = null): array
+    {
+        return $this->repository->getAll($filter);
+    }
+
+    public function findOne(string $id): stdClass|null
+    {
+        return $this->repository->findOne($id);
+    }
+
+    public function delete(string $id): bool
+    {
+        return $this->repository->delete($id);
+    }
+
+    public function create(CreateCompaniesDTO $dto): stdClass
+    {
+        return $this->repository->create($dto);
+    }
+
+    public function update(UpdateCompaniesDTO $dto): stdClass | null
+    {
+        return $this->repository->update($dto);
+    }
+}
