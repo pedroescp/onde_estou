5b0ad3470db0576fa4248c7de5789edda4056178
pedroescp <pedrohhss14@gmail.com>
2024-02-02 01:00:23 +0000

    Remove the comments

--- onde_estou_app/app/Repositories/LocationsRepositoriesInterface.php
+++ onde_estou_app/app/Repositories/LocationsRepositoriesInterface.php
@@ -2,15 +2,17 @@
 
 namespace App\Repositories;
 
+use App\DTO\Locations\CreateLocationsDTO;
+use App\DTO\Locations\UpdateLocationsDTO;
 use App\Repositories\Interfaces\PaginationInterface;
 use stdClass;
 
 interface LocationsRepositoriesInterface
 {
     public function paginate(int $page = 1, int $totalPerpage = 10, string $filter = null): PaginationInterface;
-    // public function getAll(string $filter = null): array;
-    // public function findOne(string $id): stdClass|null;
-    // public function delete(string $id): bool;
-    // public function create(CreateCompaniesDTO $dto): stdClass;
-    // public function update(UpdateCompaniesDTO $dto): stdClass|null;
+    public function getAll(string $filter = null): array;
+    public function findOne(string $id): stdClass|null;
+    public function delete(string $id): bool;
+    public function create(CreateLocationsDTO $dto): stdClass;
+    public function update(UpdateLocationsDTO $dto): stdClass|null;
 }
