b676038977a0ee4da8e3f7bcf46b603c2d5f8619
pedroescp <pedrohhss14@gmail.com>
2024-03-11 09:46:12 -0300

    Feat: remove the id

--- onde_estou_app/resources/views/companies/show.blade.php
+++ onde_estou_app/resources/views/companies/show.blade.php
@@ -52,7 +52,7 @@
                         <div class="w-full sm:w-auto">
                             <a href="{{ route('sectors.create', $companie->id) }}"
                                 class="justify-center relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 leading-5 rounded-md hover:text-gray-500 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150 w-full sm:w-auto">
-                                Adicionar novo setor {{$companie->id}}
+                                Adicionar novo setor
                             </a>
                         </div>
                     </div>
