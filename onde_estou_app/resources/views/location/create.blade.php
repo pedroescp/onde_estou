aacb29b8b47c1e420fce228c3a8f333cba9cbf9e
pedroescp <pedrohhss14@gmail.com>
2024-03-11 17:17:50 -0300

    Upgrade the selection option

--- onde_estou_app/resources/views/location/create.blade.php
+++ onde_estou_app/resources/views/location/create.blade.php
@@ -10,7 +10,7 @@
                                 {{ __('Onde você está?') }}
                             </h2>
                             <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-                                {{ __("Selecione e salve sua localização atual para aprimorar a comunicação dentro do seu setor.") }}
+                                {{ __('Selecione e salve sua localização atual para aprimorar a comunicação dentro do seu setor.') }}
                             </p>
                         </header>
                         <form method="post" action="{{ route('locations.store') }}" class="mt-6 space-y-6">
@@ -18,22 +18,48 @@
                             <div>
                                 <div>
                                     <x-input-label for="location" :value="__('Setor')" />
-                                    <select id="location" name="sector_id" onchange="updateSelectedSectorId(this.value)"
+                                    <select id="location" name="sector_id"
+                                        onchange="updateSelectedSectorId(this.value)"
                                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                                         <option selected disabled>{{ __('Selecione um setor') }}</option>
-                                        @foreach ($sectors as $sector)
+                                        {{-- @foreach ($sectors as $sector)
                                             <option value="{{ $sector->id }}">{{ $sector->name }}</option>
-                                        @endforeach
+                                        @endforeach --}}
                                     </select>
                                     <x-input-error class="mt-2" :messages="$errors->get('sector_id')" />
                                 </div>
                                 <x-primary-button class="mt-5">{{ __('Estou aqui!') }}</x-primary-button>
                             </div>
                         </form>
-                        
+
                     </section>
                 </div>
             </div>
         </div>
     </div>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $.ajax({
+                url: '/api/sectors',
+                method: 'GET',
+                success: function(response) {
+                    $('#location').empty().append(
+                        '<option selected disabled>{{ __('Selecione um setor') }}</option>');
+
+                    response.data.forEach(function(sector) {
+                        var option = $('<option>');
+                        option.val(sector.id);
+                        option.text(sector.name + ' - ' + sector.company.name);
+
+                        $('#location').append(option);
+                    });
+                },
+                error: function(xhr, status, error) {
+                    console.error(error);
+                }
+            });
+        });
+    </script>
 </x-app-layout>
