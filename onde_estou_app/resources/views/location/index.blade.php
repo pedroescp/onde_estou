3de5637027be17f30877de36663d848a4718f740
pedroescp <pedrohhss14@gmail.com>
2024-03-11 21:47:34 -0300

    Feat: add a limit time

--- onde_estou_app/resources/views/location/index.blade.php
+++ onde_estou_app/resources/views/location/index.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <div class="py-12 px-6 lg:px-0 md:px-8">
-        <div class="py-5 max-w-7xl mx-auto lg:px-8 flex flex-col sm:flex-row justify-between items-center">
+        <div class="py-5 max-w-7xl mx-auto lg:px-8 flex flex-col sm:flex-row justify-end">
 
             <!-- Barra de pesquisa -->
             {{-- <input type="text" id="table-search"
@@ -21,7 +21,7 @@
             @endauth
         </div>
 
-        <div class="max-w-7xl mx-auto lg:px-8 lg:w-full gap-4 grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1">
+        <div class="max-w-7xl mx-auto lg:px-8 lg:w-full gap-4 grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1">
             @foreach ($locationsResource as $location)
                 <x-cards :location="$location" :company="$location['company']" :sector="$location['sector']" :user="$location['nickname']" />
             @endforeach
@@ -31,4 +31,19 @@
             {{-- <x-pagination :paginator="$locations" :appends="$filters" /> --}}
         </div>
     </div>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    @if (!Auth::hasLocations())
+        <script>
+            var intervalo = 60000;
+
+            // Função para recarregar a página
+            function reloadPage() {
+                location.reload();
+            }
+
+            // Define o intervalo de recarga
+            setInterval(reloadPage, intervalo);
+        </script>
+    @endif
 </x-app-layout>
