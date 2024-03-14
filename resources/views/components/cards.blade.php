5d6780352a64b04388c94459f7aad19231e60c47
pedroescp <pedrohhss14@gmail.com>
2024-03-14 12:33:19 +0000

    Feat: change the cards

--- resources/views/components/cards.blade.php
+++ resources/views/components/cards.blade.php
@@ -7,7 +7,8 @@ class="flex flex-col items-center bg-white border border-gray-200 rounded-lg sha
         src="/docs/images/blog/image-4.jpg" alt=""> --}}
     <div class="flex flex-col justify-between p-4 leading-normal w-full">
         <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $user }}</h5>
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Localizacao: <b>{{ $sector->name }}</b> na empresa <b>{{ $company->name }}</b></p>
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 text-xs">{{ $location['updated_ago'] }}</p>
+        <p class="mb-1 text-xl font-normal text-gray-700 dark:text-gray-400">Setor: <b>{{ $sector->name }}</b></p>
+        <p class="mb-1 text-xl font-normal text-gray-700 dark:text-gray-400">Dentro da empresa: <b>{{ $company->name }}</b></p>
+        <p class="mb-1 font-normal text-gray-700 dark:text-gray-400 text-xs">{{ $location['updated_ago'] }}</p>
     </div>
 </a>
